7549efa72c9553f21bf86c6065062f174961a609
Your Name <you@example.com>
2024-10-23 21:38:05 +0530

    second commit

--- src/components/Rolldice.js
+++ src/components/Rolldice.js
@@ -11,7 +11,7 @@ const Rolldice = ({currentDice,displayDice}) => {
         onClick={displayDice}
         className='flex flex-col justify-center items-center gap-y-3 ' >
         <img src={`/images/dice-img/dice_${currentDice}.png`} alt="dice"/>
-        <p className='text-[20px] font-semibold'>Click here to roll the dice</p>
+        <p className='text-[20px] font-semibold cursor-pointer '>Click here to roll the dice</p>
         </div>
        
     </div>
